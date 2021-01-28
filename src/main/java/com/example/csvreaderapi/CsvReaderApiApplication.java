0e2ee08bbc6b7e6afb7f6322d9b2900f412cc727
sarahmorley <s.morley2@hotmail.com>
2021-01-28 21:13:51 +0000

    my first commit

---
+++ src/main/java/com/example/csvreaderapi/CsvReaderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvreaderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvReaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvReaderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csvreaderapi/CsvReaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvreaderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvReaderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
