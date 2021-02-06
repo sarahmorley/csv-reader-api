cb83c2050bfa408f76b2820f44dcd2e14b38e72a
sarahmorley <s.morley2@hotmail.com>
2021-02-06 11:27:49 +0000

    Remove unused @autowired

--- src/main/java/com/example/csvreaderapi/storage/DynamoDbStorage.java
+++ src/main/java/com/example/csvreaderapi/storage/DynamoDbStorage.java
@@ -14,9 +14,6 @@ public class DynamoDbStorage {
     @Autowired
     private AmazonDynamoDB dynamoDbClient;
 
-    @Autowired
-    private DynamoDB dynamo;
-
     final String hashKey = "CsvRowId";
     final String rangeKey = "CsvRowNumber";
 
